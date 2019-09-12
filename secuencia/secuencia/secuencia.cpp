6d6d8619f47bdece257c4ace3beca45b7eba1bf6
Alumno <Alumno@PC25>
2019-09-12 11:25:51 -0500

    holo

---
+++ secuencia/secuencia/secuencia.cpp
@@ -0,0 +1,15 @@
+// secuencia.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    
+
+
+}
+
