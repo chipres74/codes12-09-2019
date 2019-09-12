8af7b329f24ef98828a7677e0d82e18f03f6148f
Alumno <Alumno@PC25>
2019-09-12 11:49:31 -0500

    holo

--- secuencia/secuencia/secuencia.cpp
+++ secuencia/secuencia/secuencia.cpp
@@ -8,8 +8,20 @@ using namespace std;
 
 int main()
 {
-    
-
-
+	int n;
+	int suma;
+	int serie;
+	n = 0;
+	suma = 0;
+	serie = 2;
+	cout << "Introduce el ultimo valor: ";
+	cin >> n;
+	while (serie <= n){
+		cout << serie << endl;
+		suma = suma + serie;
+		serie = serie + 2;
+	}
+	cout << "La suma es: " << suma << endl;
+	
 }
 
